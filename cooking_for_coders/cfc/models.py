0dc3adef29666c7b1de8504a3dc8c7c2ea6909c2
unknown <2130048w@BO706-19.ad.dcs.gla.ac.uk>
2017-03-14 13:22:47 +0000

    models.py started

--- cooking_for_coders/cfc/models.py
+++ cooking_for_coders/cfc/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	name = models.CharField(blank=False, max_length=128, unique=False)
+	profile_picture = models.ImageField(upload_to='static/images/profile_pictures', default="static/images/default.jpg")
+	my_recipes = models.CharField(default=0, blank=True)
+	saved_recipes = models.CharField(default=0, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+class Recipe(models.Model):
+	title = models.CharField(default="Recipe", blank=False, max_length=128)
+	ingredients = models.CharField(default="ingredients", blank=False, max_length=128)
+	instructions = models.CharField(default="instructions", blank=False, max_length=128)
+	picture = models.ImageField(upload_to='static/images/recipe', blank=True)
+	rating = models.IntegerField(default=0, blank=True)
+
