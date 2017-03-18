2bf963ce9a7eeda70208820a04a714e04ab4c76b
sosaje <jonathansaje@UrNan.local>
2017-03-18 01:01:54 +0000

    fixed models, nearly complete, populate script complete

--- cooking_for_coders/cfc/models.py
+++ cooking_for_coders/cfc/models.py
@@ -5,39 +5,52 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from django.forms import ModelChoiceField
 
-class UserProfile(models.Model):
-	user = models.OneToOneField(User)
-	accountID = models.IntegerField(unique=True, blank=)
-	name = models.CharField(blank=False, max_length=128, unique=False)
-	profile_picture = models.ImageField(upload_to='static/images/profile_pictures', default="static/images/default.jpg")
-	my_recipes = models.CharField(default=0, blank=True, max_length=128)
-	saved_recipes = models.CharField(default=0, blank=True, max_length=128)
 
-	def __str__(self):
-		return self.user.username
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    accountID = models.IntegerField(unique=True, blank=False)
+    name = models.CharField(blank=False, max_length=128, unique=False)
+    profile_picture = models.ImageField(upload_to='static/images/profile_pictures', default="static/images/default.jpg")
+    my_recipes = models.CharField(default=0, blank=True, max_length=128)
+    saved_recipes = models.CharField(default=0, blank=True, max_length=128)
+
+    def __str__(self):
+        return self.user.username
+
 
 class Recipe(models.Model):
-	title = models.CharField(default="Recipe", blank=False, max_length=128)
-	ingredients = models.TextField(default="Put your ingredients here!", blank=False)
-	instructions = models.TextField(default="Type your instructions here!", blank=False)
-	picture = models.ImageField(upload_to='static/images/recipe', blank=True)
-	rating = models.IntegerField(default=0, blank=True)
-	slug = models.SlugField(blank = True, unique = True)
-	category = models.ModelChoiceField(queryset=Category.objects.all(), required=True)
-	
-	# def save(self, *args, **kwargs):
-     #    self.slug = slugify(self.title)
-	# 	#change title to title+ID?
-     #    super(Recipe, self).save(*args, **kwargs)
+    title = models.CharField(default="Recipe", blank=False, max_length=128)
+    recipeID = models.IntegerField(unique=True, blank=False)
+    ingredients = models.TextField(default="Put your ingredients here!", blank=False)
+    instructions = models.TextField(default="Type your instructions here!", blank=False)
+    picture = models.ImageField(upload_to='media/recipe/', blank=True)
+    rating = models.IntegerField(default=0, blank=True)
+    slug = models.SlugField()
+    category = models.ModelChoiceField(queryset=Category.objects.all(), required=True)
 
-	def __str__(self):
-		return self.title
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        #change title to title+ID?
+        super(Recipe, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
 
 class Category(models.Model):
-	title = models.CharField(blank=False, max_length=128)
+    title = models.CharField(blank=False, max_length=128, unique=True)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
 
 class SavedRecipe(models.Model):
+    recipe = models.ForeignKey(Recipe)
+
+    def __str__(self):
+        return self.recipe
+
+
+
+
     
--- cooking_for_coders/populate_cfc.py
+++ cooking_for_coders/populate_cfc.py
@@ -1,13 +1,68 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'cooking_for_coders.settings')
 
 import django
+
 django.setup()
-from cfc.models import Category, Recipe, UserProfile
+from cfc.models import Category, Recipe
+
 
 def populate():
-
-    Vegetarian_rec = [
-        {"title": "Spicy Courgette Python Pockets"}
+    vegetarian_rec = [
+        {"title": "Spicy Courgette Python Pockets", "recipeid": "12345",
+         "ingredients": "2 Courgettes, 1tbsp Chilli Powder, 1 Chilli, 200g Grated Cheddar",
+         "instructions": "1. Slice courgettes in half \n 2. Finely slice Chilli \n 3. Put chilli on courgettes, add chilli powder \n 4. Cook in oven for 15mins at 200degreesC"
+            , "picture": "pockets.jpg", "rating": "3", "category": "Vegetarian"}
     ]
+
+    main_rec = [
+        {"title": "Spicy Courgette Python Pockets", "recipeid": "123",
+         "ingredients": "2 Courgettes, 1tbsp Chilli Powder, 1 Chilli, 200g Grated Cheddar",
+         "instructions": "1. Slice courgettes in half \n 2. Finely slice Chilli \n 3. Put chilli on courgettes, add chilli powder \n 4. Cook in oven for 15mins at 200degreesC"
+            , "picture": "pockets.jpg", "rating": "3", "category": "Main"}
+    ]
+
+    dessert_rec = [
+        {"title": "Hot Chocolate Brownie", "recipeid": "21423",
+         "ingredients": "2 Courgettes, 1tbsp Chilli Powder, 1 Chilli, 200g Grated Cheddar",
+         "instructions": "1. Slice courgettes in half \n 2. Finely slice Chilli \n "
+                         "3. Put chilli on courgettes, add chilli powder \n 4. Cook in oven for 15mins at 200degreesC"
+            , "picture": "brownie.jpg", "rating": "5", "category": "Dessert"}
+    ]
+
+    cats = {"Vegetarian": {"recipe": vegetarian_rec},
+             "Main": {"recipe": main_rec},
+             "Dessert": {"recipe": dessert_rec}}
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat)
+        for r in cat_data["recipe"]:
+            add_rec(c, r["title"], r["recipeid"], r["ingredients"], r["instructions"], r["picture"], r["rating"], r["category"])
+
+    for c in Category.objects.all():
+        for r in Recipe.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(r)))
+
+
+def add_rec(cat, title, recipeid, ingredients, instructions, picture, rating, category):
+    r = Recipe.objects.get_or_create(category=cat, title=title) [0]
+    r.title=title
+    r.recipeid=recipeid
+    r.ingredients=ingredients
+    r.instructions=instructions
+    r.picture=picture
+    r.rating=rating
+    r.category=category
+    r.save()
+    return r
+
+def add_cat(title):
+    c = Category.objects.get_or_create(title=title) [0]
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print("Starting CFC population script...")
+    populate()
