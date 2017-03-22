7501bdddc1ac1e51da6150b675ed8ce0d1761525
sosaje <jonathansaje@UrNan.local>
2017-03-22 22:16:24 +0000

    Fixed headers not showing

--- cooking_for_coders/populate_cfc.py
+++ cooking_for_coders/populate_cfc.py
@@ -13,21 +13,21 @@ from django.utils import timezone
 
 def populate():
     vegetarian_rec = [
-        {"title": "Spicy Courgette Python Pockets", "recipeID": "12345",
+        {"title": "Spicy Courgette Python Pockets", "recipeid": "12345",
          "ingredients": "2 Courgettes, 1tbsp Chilli Powder, 1 Chilli, 200g Grated Cheddar",
          "instructions": "1. Slice courgettes in half \n 2. Finely slice Chilli \n 3. Put chilli on courgettes, add chilli powder \n 4. Cook in oven for 15mins at 200degreesC"
             ,"description":"Moist pockets of juicy, spicy courgette", "picture": "recipe/pockets.jpg", "rating": "3", "category": "Vegetarian"}
     ]
 
     lunch_rec = [
-        {"title": "Spicy Courgette Python Pockets", "recipeID": "123",
+        {"title": "Spicy Courgette Python Pockets", "recipeid": "123",
          "ingredients": "2 Courgettes, 1tbsp Chilli Powder, 1 Chilli, 200g Grated Cheddar",
          "instructions": "1. Slice courgettes in half \n 2. Finely slice Chilli \n 3. Put chilli on courgettes, add chilli powder \n 4. Cook in oven for 15mins at 200degreesC"
             ,"description":"Moist pockets of juicy, spicy courgette", "picture": "recipe/pockets.jpg", "rating": "3",  "category": "Lunch"}
     ]
 
     dessert_rec = [
-        {"title": "Hot Chocolate Brownie", "recipeID": "21423",
+        {"title": "Hot Chocolate Brownie", "recipeid": "21423",
          "ingredients": "2 Courgettes, 1tbsp Chilli Powder, 1 Chilli, 200g Grated Cheddar",
          "instructions": "1. Slice courgettes in half \n 2. Finely slice Chilli \n "
                          "3. Put chilli on courgettes, add chilli powder \n 4. Cook in oven for 15mins at 200degreesC"
@@ -41,7 +41,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for r in cat_data["recipe"]:
-            add_rec(c, r["title"], r["recipeID"], r["ingredients"], r["instructions"], r["description"], r["picture"], r["rating"], r["category"])
+            add_rec(c, r["title"], r["recipeid"], r["ingredients"], r["instructions"], r["description"], r["picture"], r["rating"], r["category"])
 
     for c in Category.objects.all():
         for r in Recipe.objects.filter(category=c):
@@ -50,8 +50,8 @@ def populate():
 now = timezone.now()
 
 
-def add_rec(cat, title, recipeID, ingredients, instructions, description, picture, rating, category):
-    r = Recipe.objects.get_or_create(category=cat, title=title, recipeID=recipeID, ingredients=ingredients, instructions=instructions, description = description)[0]
+def add_rec(cat, title, recipeid, ingredients, instructions, description, picture, rating, category):
+    r = Recipe.objects.get_or_create(category=cat, title=title, recipeID=recipeid, ingredients=ingredients, instructions=instructions, description=description)[0]
     r.picture=picture
     r.rating=rating
     r.save()
