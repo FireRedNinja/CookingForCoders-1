851a6aef8b9c1419668215b25b86f4ef0e310b86
Calum <crp97@hotmail.co.uk>
2017-03-22 16:41:25 +0000

    forms.py

--- cooking_for_coders/cfc/admin.py
+++ cooking_for_coders/cfc/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from cfc.models import Recipe, Category
+from cfc.models import Recipe, Category, UserProfile
+
 
 admin.site.register(Category)
 admin.site.register(Recipe)
