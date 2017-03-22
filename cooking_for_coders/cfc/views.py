f1e5b471679933b441b9328883bf63bc0d4c3cda
sosaje <jonathansaje@UrNan.local>
2017-03-22 22:53:54 +0000

    Adding headers, Extra index recipes

--- cooking_for_coders/cfc/views.py
+++ cooking_for_coders/cfc/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime
-from cfc.models import Recipe, UserProfile
+from cfc.models import Recipe, UserProfile, Category
 #from cfc.forms import
 
 
@@ -11,16 +11,19 @@ def index(request):
 
     try:
         # request.session.set_test_cookie()
-        # newest_recipes = Recipe.objects.filter(pub_date__lte=datetime.now()-10)order_by('-pub_date')[:4]
         top_recipes = Recipe.objects.order_by('-rating')[:4]
+        newest_recipes = Recipe.objects.order_by('-created')[:4]
         # # top 4 rated (can't do rn)
         # visitor_cookie_handler(request)
-        context_dict = {'top_recipes': top_recipes}
+        context_dict['top_recipes'] =  top_recipes
+        context_dict['newest_recipes'] = newest_recipes
 
     except Recipe.DoesNotExist:
         context_dict[top_recipes] = None
+        context_dict[newest_recipes] = None
 
     return render(request, 'cookingMain/index.html', context_dict)
+
 
 def recipe(request, recipe_id):
     try:
@@ -29,16 +32,20 @@ def recipe(request, recipe_id):
         recipe = None
     return render(request, 'cookingMain/recipe.html', {'recipe':recipe})
 
+
 def profile(request):
     return render(request, 'cookingMain/profile.html', {})
 
+
 def splash(request):
     return render(request, 'cookingMain/splash.html', {})
+
 
 def trendingRecipies(request):
     top_recipes = Recipe.objects.order_by('-rating')[:10]
     # trending recipes (can't do rn)
     return render(request, 'cookingMain/trendingRecipes.html', {})
+
 
 def savedRecipies(request):
     return render(request, 'cookingMain/savedRecipes.html', {})
